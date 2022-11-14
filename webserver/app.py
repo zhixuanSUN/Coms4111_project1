1c6d0458319331654c5068ad6ae76441e8455fa2
zhixuanSUN <72906770+zhixuanSUN@users.noreply.github.com>
2022-11-14 17:02:25 -0500

    Update app.py

--- webserver/app.py
+++ webserver/app.py
@@ -1,19 +1,13 @@
-"""
-Columbia W4111 Intro to databases
-Example webserver
-To run locally
-    python server.py
-Go to http://localhost:8111 in your browser
-A debugger such as "pdb" may be helpful for debugging.
-Read about it online.
-"""
 import os
 from sqlalchemy import *
 from sqlalchemy.pool import NullPool
 from flask import Flask, request, render_template, g, redirect, Response,url_for,flash,jsonify
+import json
 
-tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
-app = Flask(__name__, template_folder=tmpl_dir)
+#tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+#app = Flask(__name__, template_folder=tmpl_dir)
+# tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+app = Flask(__name__)
 
 DB_USER = "jz3518"
 DB_PASSWORD = "3214"
@@ -24,7 +18,6 @@ DATABASEURI = "postgresql://"+DB_USER+":"+DB_PASSWORD+"@"+DB_SERVER+"/proj1part2
 # This line creates a database engine that knows how to connect to the URI above
 #
 engine = create_engine(DATABASEURI)
-
 @app.before_request
 def before_request():
   """
@@ -139,27 +132,19 @@ def add():
     return render_template("add.html", tophat=tophat, name="add")
 
 
-if __name__ == "__main__":
-  import click
-
-  @click.command()
-  @click.option('--debug', is_flag=True)
-  @click.option('--threaded', is_flag=True)
-  @click.argument('HOST', default='0.0.0.0')
-  @click.argument('PORT', default=8111, type=int)
-  def run(debug, threaded, host, port):
-    """
-    This function handles command line parameters.
-    Run the server using
-        python server.py
-    Show the help text using
-        python server.py --help
-    """
-
-    HOST, PORT = host, port
-    print ("running on %s:%d" % (HOST, PORT))
-    app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+if __name__ == '__main__':
+    import click
 
 
+    @click.command()
+    @click.option('--debug', is_flag=True)
+    @click.option('--threaded', is_flag=True)
+    @click.argument('HOST', default='0.0.0.0')
+    @click.argument('PORT', default=8111, type=int)
+    def run(debug, threaded, host, port):
+
+        HOST, PORT = host, port
+        print("running on %s:%d" % (HOST, PORT))
+        app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
     run()
 
